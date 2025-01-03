a9bef517cb05e4c7b5df597a2aabc613c4b66aad
Tamaratare Ogu <taretamaraogu@gmail.com>
2025-01-02 20:00:13 -0600

    Update

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :first_name, presence: true 
+  validates :last_name, presence: true 
+
+  has_many :posts
+end
